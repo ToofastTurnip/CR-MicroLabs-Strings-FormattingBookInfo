6fcb6da296d008c99465f0d5140885a83da437af
iyasuwatts <iyasuwatts@iyasuwattss-MacBook-Pro.local>
2017-05-11 12:04:57 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,31 @@
+#Strings(Book Print Formatter)
+<hr>
+Complete the methods in the App Class. The test for these methods have been provided. 
+
+This program takes input from a txt file and formats the content from the file and saves said input in a new file. 
+
+You may add extra methods but are not required to complete. Any new methods should have test written for them first. 
+
+
+Example in:
+<br>
+```
+BookTitle:AuthorName:28.99
+```
+<br>
+
+Example out:
+
+`Book Name: BookTitle `
+<br>
+`AUthor: AuthorName`
+<br>
+`Price: $28.99`
+<br>
+`Price(Euros): EUR26.69`
+<br>
+`-------------------`
+
+
+
+
---
+++ src/test/java/com/zipcoder/iyasu/AppTest.java
@@ -0,0 +1,65 @@
+package com.zipcoder.iyasu;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import static com.zipcoder.iyasu.App.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testconvertDollarsToEuros() {
+        //: Given
+        String expectedEuros = "EUR0.92";
+
+        //: When
+        String actualEuros =  convertPriceToEuros(1);
+
+        //:Then
+        Assert.assertEquals(expectedEuros, actualEuros);
+
+    }
+
+    @Test
+    public void testformatToDollars(){
+        //: Given
+        String expectedDollars = "$1.00";
+
+
+        //: When
+        String actualDollars = formatToDollars(1.0);
+
+        //: Then
+        Assert.assertEquals(expectedDollars, actualDollars);
+    }
+
+    @Test
+    public void testformatLine(){
+        //:Given
+        String book = "title:author:1.00";
+        String expected = "Book Name: title\n" +
+                "Author: author\n" +
+                "Price: $1.00\n" +
+                "Price(Euros): EUR0.92"+
+                // 20 "-"
+                "\n--------------------\n";
+
+        //:When
+        String actual = formatLine(book);
+
+        //: Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testfileToList(){
+
+
+    }
+}
+
+
