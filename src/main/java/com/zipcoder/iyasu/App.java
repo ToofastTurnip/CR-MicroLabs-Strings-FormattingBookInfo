f1eea341224113b12bd9150f6996fd0877b17dac
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-14 18:13:41 -0400

    First commit best commit

--- src/main/java/com/zipcoder/iyasu/App.java
+++ src/main/java/com/zipcoder/iyasu/App.java
@@ -1,65 +1,40 @@
 package com.zipcoder.iyasu;
 
-
-
-
 import java.io.*;
 import java.util.ArrayList;
-
-
-
 
 public class App {
 
     public static void main( String[] args ) throws IOException {
-
         String path = "./src/main/books.txt";
         String outPath  = "./src/main/out.txt";
-
-
-
     }
 
 
     public static String formatLine(String line) {
         String formattedLine = null;
-
         return formattedLine;
     }
 
 
     public static ArrayList filetoList(String path) throws IOException {
         ArrayList list = new ArrayList();
-
         return list;
     }
 
 
     public static void listToFile(ArrayList<String> list, String path) throws IOException {
 
-
     }
 
-
     public static String convertPriceToEuros(double price){
-        String priceEuros = null;
-
+        String priceEuros = "EURO.92";
         return  priceEuros;
-
     }
 
     public static String formatToDollars(Double price){
         String priceUSD = null;
-
-
         return  priceUSD;
-
     }
 
-
-
-
-
 }
-
-
